9920d0b2a38afc3461f8086e3b3dc30b7b1c2a9c
Ayeon <dldkekdusz@naver.com>
2021-07-07 13:28:15 +0900

    Pod init / install

---
+++ SwipeDownClose/Pods/Target Support Files/Pods-SwipeDownCloseTests/Pods-SwipeDownCloseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwipeDownCloseTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwipeDownCloseTests
+@end
---
+++ SwipeDownClose/SwipeDownCloseTests/SwipeDownCloseTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwipeDownCloseTests.swift
+//  SwipeDownCloseTests
+//
+//  Created by 이아연 on 2021/07/07.
+//
+
+import XCTest
+@testable import SwipeDownClose
+
+class SwipeDownCloseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
